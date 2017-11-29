6f6665345220a4844cd713638b415f0e4f6b6e44
hackboxguy <albert.david@gmail.com>
2017-11-29 22:56:54 +0100

    lede-a5v11: fixed missing .config file error

--- build.sh
+++ build.sh
@@ -30,7 +30,7 @@ ln -s ../configs/$LEDE_SYSTEM_CONFIG/rootfs_overlay files #create custom-files o
 ./scripts/feeds update -a
 EXTRA_PKGS=$(cat ../configs/$LEDE_SYSTEM_CONFIG/extra_packages)
 ./scripts/feeds install $EXTRA_PKGS
-cp ../configs/$LEDE_SYSTEM_CONFIG/$LEDE_SYSTEM_CONFIG $LEDE_FOLDER/.config
+cp ../configs/$LEDE_SYSTEM_CONFIG/$LEDE_SYSTEM_CONFIG ./.config
 echo $BUILDNUMBER > files/etc/version.txt #include lede-a5v11 version number file
 make defconfig
 make -j41 #O=$LEDE_OUTPUT_FOLDER -j41 #$BR_BOARD_CONFIG BRBOX_RELVERSION=$IMAGE_VERSION BRBOX_BUILDNUM=$TMP_BUILDNUM BRBOX_SYSCONFIG=$BR_BOARD_SYSTEM_CONFIG
