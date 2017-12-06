58e2b9c058ff9b41f947a9e40e282505c99370a0
hackboxguy <albert.david@gmail.com>
2017-12-06 22:25:57 +0100

    lede-a5v11: included sysmgr rpc service package

--- configs/a5v11-base/update-package-index.sh
+++ configs/a5v11-base/update-package-index.sh
@@ -11,3 +11,7 @@ cat ../packages/AdDisp/pkg.index >> feeds/packages.index
 #libAdCmn
 ln -s ../../../../packages/AdCmn feeds/packages/libs/AdCmn
 cat ../packages/AdCmn/pkg.index >> feeds/packages.index
+
+#sysmgr
+ln -s ../../../../packages/sysmgr feeds/packages/utils/sysmgr
+cat ../packages/sysmgr/pkg.index >> feeds/packages.index
