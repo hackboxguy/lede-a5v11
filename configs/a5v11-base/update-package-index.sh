a0f4fbae16a235449dcd0805b03b3cb56229e14e
hackboxguy <albert.david@gmail.com>
2017-12-06 08:27:26 +0100

    lede-a5v11: fixed soft link creation for AdSttngs package

--- configs/a5v11-base/update-package-index.sh
+++ configs/a5v11-base/update-package-index.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ln -s ../packages/AdSttngs feeds/packages/libs/AdSttngs
+ln -s ../../../../packages/AdSttngs feeds/packages/libs/AdSttngs
 cat ../packages/AdSttngs/pkg.index >> feeds/packages.index
