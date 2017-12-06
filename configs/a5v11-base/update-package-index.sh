14f5cf73a22f35fd3b35a4f07ddf50e20a8ffbc9
hackboxguy <albert.david@gmail.com>
2017-12-06 17:17:48 +0100

    lede-a511: included libAdDisp library package

--- configs/a5v11-base/update-package-index.sh
+++ configs/a5v11-base/update-package-index.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
+
+#libAdSttngs
 ln -s ../../../../packages/AdSttngs feeds/packages/libs/AdSttngs
 cat ../packages/AdSttngs/pkg.index >> feeds/packages.index
+
+#libAdDisp
+ln -s ../../../../packages/AdDisp feeds/packages/libs/AdDisp
+cat ../packages/AdDisp/pkg.index >> feeds/packages.index
